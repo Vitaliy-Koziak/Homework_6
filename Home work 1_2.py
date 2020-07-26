7c883e367b4367c84f34bfbb4c0047f700d88423
Vitaliy-Koziak <kozyakvit@gmail.com>
2020-07-26 15:30:11 +0300

    task is made

--- Home work 1_2.py
+++ Home work 1_2.py
@@ -1,2 +1,18 @@
 #Заповнити список дійсними числами введенням з клавіатури. Порахувати суму і добуток елементів списку. Вивести на екран сам список, отримані
 #суму і добуток його елементів.
+m = int(input("Enter size of list: "))
+array = []
+print("Enter %d elements: "%m)
+for i in range(m):
+    array.append(float(input()))
+sum = 0
+prod = 1
+for item in array:
+    print("%.2f "%item,end="")
+print()
+for item in array:
+    sum+=item
+print("Sum of elements = %.2f"%sum)
+for item in array:
+    prod*=item
+print("Product of elements = %.2f"%prod)
